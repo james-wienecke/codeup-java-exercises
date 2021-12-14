dcec823997a8fe914ad03fc9fd6936483915a0f0
James Wienecke <james.a.wienecke@gmail.com>
2021-12-14 12:21:34 -0600

    Split hello world print to two lines of code, outputing to just one line.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
     public static void main (String[] args) {
-        System.out.println("Hewwow, World!");
+        System.out.print("Hewwow, ");
+        System.out.print("World!");
     }
 }
