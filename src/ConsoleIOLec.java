118808670ea8d7d88a3b38e0e429a5b45895e45a
James Wienecke <james.a.wienecke@gmail.com>
2021-12-15 10:26:36 -0600

    Continuing to experiment with formatted string output.

--- src/ConsoleIOLec.java
+++ src/ConsoleIOLec.java
@@ -1,7 +1,12 @@
 public class ConsoleIOLec {
     public static void main (String[] args) {
-        String name = "James";
+        String firstName = "James";
+        String lastName = "Wienecke";
+        int favNum = 26;
+        double pi = 3.141592653;
 
-        System.out.printf("Hello, my name is %s", name);
+        System.out.printf("Hello, my name is %s %s. My favorite number is %n", firstName, lastName);
+        System.out.printf("My favorite number is %d! Pi to 6 decimal precision is %.6f...", favNum, pi);
+        System.out.printf("Pi to 2 digits of decimal precision is %.2f", pi);
     }
 }
