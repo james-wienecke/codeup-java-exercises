ddfb04a9b463bc5e39b7ba5a6d0a28aec50f5c83
James Wienecke <james.a.wienecke@gmail.com>
2021-12-15 10:54:21 -0600

    Setting up workspace for console IO exercises and formatting string exercises.

---
+++ src/ConsoleExercises.java
@@ -0,0 +1,37 @@
+public class ConsoleExercises {
+    public static void main (String[] args) {
+        /*
+        Copy this code into your main method:
+        double pi = 3.14159;
+        Write some Java code that uses the variable pi to output the following:
+
+        The value of pi is approximately 3.14.
+        Don't change the value of the variable; instead, reference one of the links above
+        and use System.out.format to accomplish this.
+        */
+        double pi = 3.14159;
+        System.out.printf("The value of pi is approximately %.2f", pi);
+
+        /* Explore the Scanner Class.
+        Prompt a user to enter an integer and store that value in an int variable using the nextInt method. */
+
+        /* What happens if you input something that is not an integer?
+        Prompt a user to enter 3 words, and store each of them in a separate variable.
+        Then, display them back in the console, each on a newline. */
+
+        // What happens if you enter less than 3 words?
+
+        // What happens if you enter more than 3 words?
+
+
+        /* Prompt a user to enter a sentence, then store that sentence in a String variable
+        using the next method. Then, display that sentence back to the user. */
+
+        // do you capture all the words?
+        // Rewrite the above example using the nextLine method.
+
+
+
+
+    }
+}
