d02d9b0375bff58cf68d4b891db6cbe7a6400d0e
James Wienecke <james.a.wienecke@gmail.com>
2021-12-16 10:47:49 -0600

    Finished exercise 2.

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -11,11 +11,31 @@ public class ControlFlowExercises {
         }
 
         // newline for the power loop
-        System.out.print("\n");
+        System.out.print('\n');
 
         // do while refactored to for
         for (int i = 2; i <= 1_000_000; i = (int) Math.pow(i, 2)) {
             System.out.printf("%d\n", i);
+        }
+
+        /* FizzBuzz */
+        //One of the most common interview questions for entry-level programmers is the FizzBuzz test.
+        // Developed by Imran Ghory, the test is designed to assess basic looping and conditional logic skills.
+
+        //Write a program that prints the numbers from 1 to 100.
+        //For multiples of three: print “Fizz” instead of the number.
+        //For the multiples of five: print “Buzz”.
+        //For numbers which are multiples of both three and five: print “FizzBuzz”.
+        for (int i = 1; i <= 100; i++) {
+            if ((i % 3 == 0) && (i % 5 == 0)) {
+                System.out.println("FizzBuzz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else {
+                System.out.println(i);
+            }
         }
     }
 }
