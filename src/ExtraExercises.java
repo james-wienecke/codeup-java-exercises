14a40974ca08a9e1938f30c72bb35f77201d22e9
James Wienecke <james.a.wienecke@gmail.com>
2022-01-03 13:56:17 -0600

    Cleanup and presentation.

--- src/ExtraExercises.java
+++ src/ExtraExercises.java
@@ -10,7 +10,6 @@ public class ExtraExercises {
         The sum of 10 numbers is : 55
         The Average is : 5.500000
          */
-        /*
         int[] numbers = new int[10];
         System.out.println("You will be prompted for ten integers. After all are received, you will be provided with their sum and average.");
         for (short i = 0; i < 10; i++) {
@@ -19,7 +18,7 @@ public class ExtraExercises {
         }
         System.out.printf("Sum of numbers: %d\n", getIntArraySum(numbers));
         System.out.printf("Average of numbers: %d\n", getAverage(numbers));
-        */
+
         /* Write a program in Java to display the cube of the number upto given an integer. Read the number using the Scanner class.
 
         Expected Output :
@@ -29,13 +28,11 @@ public class ExtraExercises {
         Number is : 4 and cube of the 4 is :64
         Number is : 5 and cube of the 5 is :125
          */
-        /*
         System.out.println("Enter an integer and all cubes from 1 to your integer will be displayed.");
         int cubeNum = sc.nextInt();
         for (int i = 1; i <= cubeNum; i++) {
             System.out.printf("Number is: %d and its cube is: %d\n", i, (long) Math.pow(i, 3));
         }
-        */
 
         /* Write a program in Java to display the n terms of odd natural number and their sum. Read the odd number using the Scanner class
 
@@ -45,7 +42,6 @@ public class ExtraExercises {
         The odd numbers are :1 3 5 7 9 11 13 15 17 19
         The Sum of odd Natural Number upto 10 terms : 100
          */
-        /*
         System.out.println("Enter the number of terms for display of odd natural numbers from 1 and their sum.");
         int oddTerms = sc.nextInt();
         int oddSum = 0;
@@ -55,7 +51,7 @@ public class ExtraExercises {
             oddSum += i;
         }
         System.out.printf("\nSum of odd natural numbers up to %d terms: %d\n", oddTerms, oddSum);
-        */
+
         /* Write a program in Java to display the pattern like right angle triangle with a number. Read the number using the Scanner class.
 
         The pattern like :
@@ -64,7 +60,6 @@ public class ExtraExercises {
         123
         1234
          */
-        /*
         System.out.println("Enter a number to determine the size of the right angle triangle.");
         int triangleNum = getInteger(0, Integer.MAX_VALUE);
         for (int i = 1; i <= triangleNum; i++) {
@@ -73,7 +68,6 @@ public class ExtraExercises {
             }
             System.out.println();
         }
-         */
 
         /* Write a program in Java to make such a pattern like right angle triangle with number increased by 1.
 
