3f3fea4fa2a74dd102ec633e3e0f612ba30c6a0a
James Wienecke <james.a.wienecke@gmail.com>
2022-01-05 10:37:07 -0600

    Added getInt and getDouble. Reused and spruced up some of my old code.

--- src/util/Input.java
+++ src/util/Input.java
@@ -27,4 +27,44 @@ public class Input {
             return true;
         } else return false;
     }
+
+    int getInt() {
+        int input;
+        try {
+            input = sc.nextInt();
+        } catch (Exception e) {
+            input = getInt();
+        }
+        return input;
+    }
+
+    int getInt(int min, int max) {
+        int userInt = getInt();
+        if (min > max)
+            return getInt(max, min);
+        if (userInt < min || userInt > max)
+            return getInt(min, max);
+        else
+            return  userInt;
+    }
+
+    double getDouble() {
+        double input;
+        try {
+            input = sc.nextDouble();
+        } catch (Exception e) {
+            input = getDouble();
+        }
+        return input;
+    }
+
+    double getDouble(double min, double max) {
+        double input = getDouble();
+        if (min > max)
+            return getDouble(max, min);
+        if (input < min || input > max)
+            return getDouble(min, max);
+        else
+            return  input;
+    }
 }
