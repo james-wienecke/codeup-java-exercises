c1c494017891e40717ceb6f4f41fc9656c72fabe
James Wienecke <james.a.wienecke@gmail.com>
2022-01-05 11:35:19 -0600

    Added a new field to keep track of total number of circles created. Brought in a new method to help loop through the circle creation step until user indicated they want to stop.

--- src/shapes/CircleApp.java
+++ src/shapes/CircleApp.java
@@ -3,8 +3,18 @@ import util.Input;
 
 public class CircleApp {
     private static Input in = new Input();
+    private static int totalCircles = 0;
+
     public static void main(String[] args) {
+        do {
+            createCircle();
+        } while(in.yesNo("Would you like to create another circle?"));
+        System.out.println(String.format("Circles created: %d", getTotalCircles()));
+    }
+
+    private static void createCircle() {
         Circle circle = new Circle(in.getDouble("Enter a radius of a circle:"));
+        incrementTotalCircles();
         System.out.println(printCircleDetails(circle));
     }
 
@@ -12,5 +22,13 @@ public class CircleApp {
         return String.format("Circumference: %f%nArea: %f",
                 c.getCircumference(),
                 c.getArea());
+    }
+
+    public static int getTotalCircles() {
+        return totalCircles;
+    }
+
+    public static void incrementTotalCircles() {
+        totalCircles++;
     }
 }
