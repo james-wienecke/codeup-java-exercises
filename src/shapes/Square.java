56335fe428d0e47b9d65c8471bb5853b918d3010
James Wienecke <james.a.wienecke@gmail.com>
2022-01-07 14:31:56 -0600

    Fixed issue with Shape subclass method definitions. The recommendation from intellij overloaded them instead of overriding the superclass's methods.

--- src/shapes/Square.java
+++ src/shapes/Square.java
@@ -4,4 +4,12 @@ public class Square extends Rectangle {
     public Square(double side) {
         super(side, side);
     }
+
+    public double getPerimeter() {
+        return this.width * 4;
+    }
+
+    public double getArea() {
+        return Math.pow(this.width, 2);
+    }
 }
