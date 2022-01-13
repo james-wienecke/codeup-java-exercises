1c178bc7a2369de4d9bd78e4e6e6f929d8c63aac
James Wienecke <james.a.wienecke@gmail.com>
2022-01-13 10:47:10 -0600

    Following along with lecture.

--- src/ExceptionsErrorHandlingLec.java
+++ src/ExceptionsErrorHandlingLec.java
@@ -1,10 +1,31 @@
 public class ExceptionsErrorHandlingLec {
     public static void main(String[] args) {
+//        try {
+//            throw new Exception("No, Billy! Don't throw an exception!");
+//        } catch (Exception e) {
+//            e.printStackTrace();
+//            System.out.println("Can still do stuff afterwards!");
+//        }
+//        try {
+//            System.out.println("Let's see...");
+//            int result = 1 / 0;
+//            System.out.println("I can divide by zero!");
+//        } catch (ArithmeticException e) {
+//            System.err.println("Math still works!");
+//        }
+        int[] numbers = {1, 2, 3};
+        int x = 0;
+
         try {
-            throw new Exception("No, Billy! Don't throw an exception!");
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.out.println("Can still do stuff afterwards!");
+            x = numbers[5];
+        } catch(ArrayIndexOutOfBoundsException e) {
+            System.err.println("NO");
+        } catch(Exception e) {
+            System.err.println("I am a generic catch");
+        } finally {
+            System.out.println("This always runs");
         }
+
+        System.out.println(x);
     }
 }
