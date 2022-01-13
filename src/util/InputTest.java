d81f042d2abd5e11234bb161c8e22977622c2fcc
James Wienecke <james.a.wienecke@gmail.com>
2022-01-13 11:53:12 -0600

    Added tests for the new methods added to util.Input.

--- src/util/InputTest.java
+++ src/util/InputTest.java
@@ -38,5 +38,27 @@ public class InputTest {
         double myDoubleBoundedPrompt = in.getDouble(-2047.325, 3345.0497, "Enter a decimal number between -2047.325 and 3345.0497");
         System.out.println(myDoubleBoundedPrompt);
 
+
+        // brand new binary input methods!
+        System.out.println("Testing getBinary");
+        int myBinary = in.getBinary();
+        System.out.println(myBinary);
+        int myBinaryPrompt = in.getBinary("Enter a binary number");
+        System.out.println(myBinaryPrompt);
+        int myBinaryBounded = in.getBinary(3, 10);
+        System.out.println(myBinaryBounded);
+        int myBinaryBoundedPrompt = in.getBinary(3, 9, "Enter a binary number between 3 and 9");
+        System.out.println(myBinaryBoundedPrompt);
+
+        // getting some hexes and they're hot hot hot!!!
+        System.out.println("Testing getHex");
+        int myHex = in.getHex();
+        System.out.println(myHex);
+        int myHexPrompt = in.getHex("Enter a hex number");
+        System.out.println(myHexPrompt);
+        int myHexBounded = in.getHex(3, 10);
+        System.out.println(myHexBounded);
+        int myHexBoundedPrompt = in.getHex(3, 9, "Enter a hex number between 3 and 9");
+        System.out.println(myHexBoundedPrompt);
     }
 }
